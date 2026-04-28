266a1f72fab6c37c6cbf22302f486ed1b6721549
yasoo <you@example.com>
2026-04-29 03:08:52 +0900

    Fix sitemap config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { sitemap } from 'vite-plugin-sitemap'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
